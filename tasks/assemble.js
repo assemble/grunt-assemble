0de9a254771a88230d4bd68d880808168ff09df0
jonschlinkert <dev@sellside.com>
2014-05-10 19:45:25 -0400

    update deps, minor code edits

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -9,7 +9,8 @@
 var file = require('fs-utils');
 var async = require('async');
 var assemble = require('assemble');
-//var _str = require('underscore.string');
+// var load = require('resolve-dep');
+// var _str = require('underscore.string');
 var _ = require('lodash');
 var normalize = require('../lib/normalize');
 
@@ -21,13 +22,12 @@ module.exports = function (grunt) {
   grunt.util.linefeed = '\n';
 
   grunt.registerMultiTask('assemble', 'Compile template files with specified engines', function () {
-
     var done = this.async();
 
     grunt.log.writeln(); // empty line
 
     // normalize grunt options data into assemble metadata
-    var config = normalizeOptions(grunt, this, assemble.defaults);
+    var options = normalizeOptions(grunt, this, assemble.defaults);
 
     // normalize grunt files object into assemble files
     normalize.files(this, function (err, files) {
@@ -37,15 +37,23 @@ module.exports = function (grunt) {
         return done(false);
       }
 
-      config.files = files;
-      config.grunt = grunt;
+      options.files = files;
+      options.grunt = grunt;
+
+      // mix methods from underscore.string into lodash.
+      // _.mixin(_str);
+
+      // var mixins = load(grunt.config.process(options.mixins));
+      // mixins.forEach(function(mixin) {
+      //   _.mixin(require(mixin));
+      // })
 
       // if there are command line options set, use those
-      config.log = config.log || {};
-      config.log.level = logOpts || config.log.level || 'error';
+      options.log = options.log || {};
+      options.log.level = logOpts || options.log.level || 'error';
 
-      // configure assemble and build
-      assemble(config).build(function (err, results) {
+      // optionsure assemble and build
+      assemble(options).build(function (err, results) {
 
         if (err) {
           grunt.warn(err);
@@ -54,14 +62,13 @@ module.exports = function (grunt) {
 
         // write out the resulting pages
         var keys = _.keys(results.pages);
-
         async.each(keys, function (key, nextPage) {
           var page = results.pages[key];
           grunt.log.writeln('  writing'.green, page.dest);
 
-          if (config.debug) {
+          if (options.debug) {
             grunt.verbose.writeln(page);
-            file.writeFileSync(config.debug || 'tmp/debug-page.json', page);
+            file.writeFileSync(options.debug || 'tmp/debug-page.json', page);
           }
 
           file.writeFileSync(page.dest, page.content);
@@ -81,6 +88,7 @@ module.exports = function (grunt) {
     return _.union(file.arrayify(taskArray), file.arrayify(targetArray));
   };
 
+
   /**
    * normalize target.options into options
    * and metadata that assemble can use
@@ -88,22 +96,21 @@ module.exports = function (grunt) {
    * @return {[type]}        [description]
    */
   var normalizeOptions = function (grunt, target, defaults) {
-
     // get the options from the target, with defaults
     defaults = defaults || {};
     var options = target.options(defaults);
 
     // merge task and target arrays
-    options = _.mapValues(options, function (value, key) {
+    options = _.mapValues(options, function (value, option) {
 
       // make sure some values are arrays
-      if (key === 'partials') {
+      if (option === 'partials') {
         value = file.arrayify(value);
       }
 
       // if the value is an array them
       if (_.isArray(value)) {
-        return mergeOptions(key, grunt);
+        return mergeOptions(option, grunt);
       }
       return value;
     });
@@ -111,5 +118,4 @@ module.exports = function (grunt) {
     // return options
     return options;
   };
-
 };
